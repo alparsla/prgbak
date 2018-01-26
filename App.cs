711796c99bf4b71181a9689ddf5ba55e594cf04d
alparsla <5493908+alparsla@users.noreply.github.com>
2018-01-26 15:39:56 +0300

    Added App class as entry point and get HomePath

---
+++ App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace PrgBak
+{
+	internal class App
+	{
+		private static string homePath;
+		
+		static void Main()
+		{
+			App.homePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + Path.DirectorySeparatorChar + ".prgbak" + Path.DirectorySeparatorChar;	
+		}
+
+		internal static string HomePath
+		{
+			get
+			{
+				return App.homePath;				
+			}
+		}
+		
+	}
+}
---
+++ Log.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace PrgBak
+{
+	internal class Log
+	{
+		private static string path;
+		
+		static Log()
+		{
+		}
+		
+		internal static void Print(object o)
+		{
+			
+		}
+	}
+}
